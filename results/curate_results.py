62d3e943d7f44a865c9c15a4e6ff02053ec9834d
Chris Crabtree <chris414862@gmail.com>
2021-10-23 09:31:52 -0500

    Minor changes to results display script

--- results/curate_results.py
+++ results/curate_results.py
@@ -2,6 +2,7 @@ import argparse
 import re
 from pathlib import Path
 import pandas as pd
+import sys
 
 
 parser = argparse.ArgumentParser(description='Produce Results in table form')
@@ -25,6 +26,9 @@ def colate_cols(regex_list, sorted_cols):
     return colated_cols
 
 def get_col_sequence(sorted_cols, sim_type='dot'):
+    """
+        Organizes the sequence of columns in the table
+    """
     regex_list = [r".*image.*"+sim_type+"_avg_r1$", r".*image.*"+sim_type+"_avg_r5$", r".*image.*"+sim_type+"_avg_r10$"]
     img_avg_cols = colate_cols(regex_list, sorted_cols)
 
@@ -59,10 +63,12 @@ for exp_dir in exp_dirs:
 exp_best_ep_dict = {}
 use_cos_sim = True
 row_data = {}
+pd.set_option("display.max_rows", 500)
 for exp_dir, d in exp_df_dict.items():
     df, descrip = d['df'], d['descrip']
-    best_epoch = int(df.iloc[-1]["best_epoch"])
+    best_epoch = int(df.iloc[-1]["best_epoch"])-1
     sorted_cols = [item[0] for item in sorted(df.iloc[best_epoch].iteritems(), key=lambda x: x[0]) if re.match(r"^.*(_avg_|_dot_|_cos_)", item[0]) is not None]
+    sorted_cols = [c for c in sorted_cols if not re.search(r"_norm_dot_", c)]
      
     col_sequence = get_col_sequence(sorted_cols, sim_type="dot")
     if not args.omit_cos_sim:
@@ -70,10 +76,17 @@ for exp_dir, d in exp_df_dict.items():
 
 
     best_ep_results = df.iloc[best_epoch]
-    best_ep_results = best_ep_results.append(pd.Series({"Description":descrip}))
+    # Additional info
+    add_info = pd.Series({
+                "Description":descrip,
+                "Best Epoch":best_epoch
+                })
+    add_info_keys = add_info.index.tolist()
+    best_ep_results = best_ep_results.append(add_info)
 
-    col_sequence = ["Description"]+col_sequence
+    col_sequence = add_info_keys+col_sequence
     curated_ep_results = best_ep_results[col_sequence]
+
 
     reduced_col_sequence = []
     for col in col_sequence:
@@ -84,12 +97,15 @@ for exp_dir, d in exp_df_dict.items():
         reduced_col_sequence.append(col)
 
     curated_ep_results.index = reduced_col_sequence
+    # Key is experiment directory name, value is row of results
     row_data[exp_dir.parts[-1]] = curated_ep_results
 
 
+# Orientating by index to make formating easier by column 
+# Pandas has special enforcement rules for data types in columns, so changing dt by rows is difficult
 df = pd.DataFrame.from_dict(row_data, orient="index")
 for col in df.columns:
-    if col == "Description":
+    if col in add_info_keys:
         continue
     df[col] = pd.Series([f"{val:.2%}" for k, val in df[col].iteritems()], index=df.index)
 
